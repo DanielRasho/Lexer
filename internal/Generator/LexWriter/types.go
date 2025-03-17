6523dc379c2825d3747930b10f844c555f6a6932
DanielRasho <ray22933@uvg.edu.gt>
2025-03-16 20:34:59 -0600

    feat: definition of Lexer template

--- internal/Generator/LexWriter/types.go
+++ internal/Generator/LexWriter/types.go
@@ -1,3 +1,10 @@
 package Lex_writer
 
 // This module is in charge of writing the final Lexer.go file, based on a template file
+
+// Definition of variable fields withing a template
+type LexTemplate struct {
+	Header   string
+	Automata string
+	Footer   string
+}
