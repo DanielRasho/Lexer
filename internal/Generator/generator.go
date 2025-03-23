ce954e7e6423710d87c09bc8ec0a1ea76531c0ed
DanielRasho <ray22933@uvg.edu.gt>
2025-03-23 11:02:34 -0600

    feat: rename generator file

--- internal/Generator/generator.go
+++ internal/Generator/generator.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strconv"
 
+	dfa "github.com/DanielRasho/Lexer/internal/DFA"
 	balancer "github.com/DanielRasho/Lexer/internal/DFA/Balancer"
 	postfix "github.com/DanielRasho/Lexer/internal/DFA/Postfix"
 	yalex_reader "github.com/DanielRasho/Lexer/internal/Generator/YALexReader"
@@ -22,6 +23,13 @@ func Compile(filePath, outputPath string) error {
 	rawExpresion := make([]postfix.RawSymbol, 0)
 
 	for index, rule := range yalexDefinition.Rules {
+		// For special tokens (the ones encapsulating actionable code)
+		// to be diferentiable they must:
+		// 	- Have more than 1 char
+		//	- Be unique for each special symbol
+		// This is to ensure they are no mixed up with other common symbols
+		// Therefore a easy technique is to assign them an id starting in 10.
+		startIndex := 10
 
 		ok, _ := balancer.IsBalanced(rule.Pattern)
 		if !ok {
@@ -34,14 +42,20 @@ func Compile(filePath, outputPath string) error {
 		}
 		rawExpresion = append(rawExpresion, postfix.RawSymbol{Value: ")"})
 		rawExpresion = append(rawExpresion, postfix.RawSymbol{
-			Value: strconv.Itoa(index), HasAction: true, Action: rule.Action})
+			Value: strconv.Itoa(index + startIndex), HasAction: true, Action: rule.Action})
 
 		if index != len(yalexDefinition.Rules)-1 {
 			rawExpresion = append(rawExpresion, postfix.RawSymbol{Value: "|"})
 		}
 
 	}
+
+	for _, v := range rawExpresion {
+		fmt.Print(v.Value)
+	}
+
 	// Generate DFA for language recognition
+	dfa.NewDFA(rawExpresion)
 
 	// Simplify
 
