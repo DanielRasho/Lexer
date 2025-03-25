188f17096f663ff2065175b6e292fe870db35ca5
cmd-AJ <andrewlol774@gmail.com>
2025-03-25 15:25:26 -0500

    Trying to fix an issue on the main.go

--- cmd/Lexer/main.go
+++ cmd/Lexer/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+)
 
+func main() {
+	lexer, err := NewLexer("./examples/test1.yaa")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	for {
+		token, err := lexer.GetNextToken()
+		if err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+		fmt.Print(token.String() + "\n")
+	}
 }
